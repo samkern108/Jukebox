1ad8cb719c9eaa3d2dde66db88bb77bccb5a1d8b
Sam Kern <samkern108@gmail.com>
2017-01-26 18:41:59 -0800

    So uh, arrow animation was moved down.

--- Assets/Scripts/Game Management/L1_Instructions.cs
+++ Assets/Scripts/Game Management/L1_Instructions.cs
@@ -26,21 +26,14 @@ public class L1_Instructions : MonoBehaviour {
 	public void NextStage() {
 		switch (stage) {
 		case 0: 
-			//this is the ui element
 			RectTransform circleTransform = circle.GetComponent<RectTransform>();
-
-			//first you need the RectTransform component of your canvas
 			RectTransform CanvasRect = GetComponent<Canvas>().GetComponent<RectTransform>();
 
-			//then you calculate the position of the UI element
-			//0,0 for the canvas is at the center of the screen, whereas WorldToViewPortPoint treats the lower left corner as 0,0. Because of this, you need to subtract the height / width of the canvas * 0.5 to get the correct position.
-
-			Vector2 ViewportPosition=Camera.main.WorldToViewportPoint(BeatMaster.GetClosestGridCell(3, 3));
+			Vector2 ViewportPosition=Camera.main.WorldToViewportPoint(BeatMaster.GetClosestGridCell(3, 1));
 			Vector2 WorldObject_ScreenPosition=new Vector2(
-				((ViewportPosition.x*CanvasRect.sizeDelta.x)-(CanvasRect.sizeDelta.x*0.5f)),
-				((ViewportPosition.y*CanvasRect.sizeDelta.y)-(CanvasRect.sizeDelta.y*0.5f)));
+				((ViewportPosition.x*CanvasRect.sizeDelta.x)-(CanvasRect.sizeDelta.x * 0.5f)),
+				((ViewportPosition.y*CanvasRect.sizeDelta.y)-(CanvasRect.sizeDelta.y * 0.5f)));
 
-			//now you can set the position of the ui element
 			circleTransform.anchoredPosition = WorldObject_ScreenPosition;
 
 			circle.SetActive (true);
